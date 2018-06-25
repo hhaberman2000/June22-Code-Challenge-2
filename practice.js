9d8ce24c3f440995dee381e947ae384ae4e63673
Hal Haberman <hhaberman@techtonicgroup.com>
2018-06-25 13:17:57 -0600

    commit final practice.js

--- practice.js
+++ practice.js
@@ -1,45 +1,38 @@
-
-
-
-// var str ="arrb6???4xxbl5???eee5";
-// qcount=0
-// y = 0;
+document.addEventListener("DOMContentLoaded", function() {
+  window.QuestionMarks("bb6?9")
+});
 
 function  QuestionMarks(str){
-  var str ="arrb6???4xxbl5???eee5";
-  qcount=0
-  y = 0;
+
+  var qcount=0;
+  var y = 0;
+  var strIsValid = false;
   for (var i=0; i <str.length;) {
       if ( isNaN(str.charAt(i)) && str.charAt(i) != "?") {
-        x = str.charAt(i);
-        console.log("not a number" + x);
+        var x = str.charAt(i);
         i++;
       }
         else if (str.charAt(i) === "?" ) {
-            q = str.charAt(i);
+            var q = str.charAt(i);
             qcount = qcount + 1;
             i++;
-            console.log(q + qcount);
         }
         else {
           x = str.charAt(i);
           var integer = parseInt(x ,10);
-          console.log(integer);
           i++;
           if ( integer > 0) {
             y = y + integer;
           }
         }
         if ( y == 10 && qcount == 3){
-          console.log("true");
-          return true;
+          y = 0;
+          qcount = 0;
+          return strIsValid = true;
+          break;
         }
-          else if (i == str.length - 1){
-           console.log("false");
-           return false;
-          }
+        if (y == 0 ) {
+          qcount = 0;
+        }
       }
 }
-document.addEventListener("DOMContentLoaded", function() {
-  window.gQuestionMarks = new QuestionMarks();
-});
